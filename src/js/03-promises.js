3173f3af3ef84eb02e68813772c4ad2ae83985e1
Anton Kit <croixua@gmail.com>
2022-02-07 00:22:01 +0200

    task-03 done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,37 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const formRef = document.querySelector('form');
+const inputsRef = document.querySelectorAll('input');
+const delayRef = inputsRef[0];
+const stepRef = inputsRef[1];
+const amountRef = inputsRef[2];
+let defaultCount = 1;
+let id = null;
+
+formRef.addEventListener('submit', callPromise);
+
+function callPromise(e) {
+  e.preventDefault();
+
+  setTimeout(() => {
+    id = setInterval(() => {
+      if (defaultCount === +amountRef.value) {
+        clearInterval(id);
+      }
+      createPromise(defaultCount, stepRef.value).then().catch();
+      defaultCount++;
+    }, stepRef.value);
+  }, delayRef.value);
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+  return new Promise((resolve, reject) => {
+    if (shouldResolve) {
+      resolve(onSucces(position, delay));
+    } else {
+      reject(onError(position, delay));
+    }
+  });
 }
